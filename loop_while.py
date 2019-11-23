435ff7972959239cf82dd20e43228a269141aaee
anselmo <anselmonnvix@yahoo.com>
2019-11-23 00:00:33 -0300

    Uso do loop while close #4

---
+++ loop_while.py
@@ -0,0 +1,33 @@
+"""
+Loop while
+
+forma geral
+
+while expressão_booleana:
+    //execução do loop
+
+o bloco do while será repedito enquanto a booleana for verdadeira.
+
+expressão Booleana é toda expressão onde o resultado é verdadeiro ou faldo
+
+# Exemplo
+
+num = 5
+
+num < 5
+
+# exemplo 1
+
+numero = 1
+
+while numero < 10:
+    print(numero)
+    numero = numero + 1
+
+# OBS: Em um while, é importante que cuidemos do critério de parada.
+"""
+
+# Exemplo 2
+resposta = ''
+while resposta != 'sim':
+    resposta = input('já acabou Jéssica: ')
