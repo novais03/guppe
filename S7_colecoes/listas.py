7eb04587c3c998242a63f60d44b4717a5e5cdde4
anselmo <anselmonnvix@yahoo.com>
2019-11-26 23:19:16 -0300

    Uso de listas

--- S7_colecoes/listas.py
+++ S7_colecoes/listas.py
@@ -39,6 +39,17 @@ print(lista5.count('e'))
 
 # Adicionar elementos em listas
 
+type([])
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 's', 'i', 't', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Geek University')
 
 Para adicionar elementos em listas, utilizamos a função append
 
@@ -67,17 +78,194 @@ print(lista1)
 # OBS: isso não substitui o valor inicial. O mesmo será deslocado para a direita da lista
 lista1.insert(2, 'Novo Valor')
 print(lista1)
-"""
-type([])
-
-lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
-
-lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 's', 'i', 't', 'y']
-
-lista3 = [[]]
-
-lista4 = list(range(11))
-
-lista5 = list('Geek University')
 
 # Podemos facilmente juntar duas listas
+
+# lista6 = lista1 + lista2
+lista1.extend(lista2)
+print(lista1)
+
+# Podemos facilamente inverter uma lista
+
+# Forma 1
+lista1.reverse()
+lista2.reverse()
+print(lista1)
+print(lista2)
+
+# Forma 2
+print(lista1[::-1])
+print(lista2[::-1])
+
+# Copiar uma lista
+
+lista6 = lista2.copy()
+print(lista6)
+
+# Quantos elementos tem numa lista
+print(len(lista2))
+# Podemos remover facilmente o último elemento de um lista
+#Obs: O pop não somente remove o útlimo elemento mais também o retorna
+print(lista5)
+lista5.pop()
+print(lista5)
+
+# Podemos remover um elemento pelo índice
+# OBS: Os elementos á direita deste índice serão deslocados para a esquerda
+# OBS: Se não houver elemento no índice informado teremos o erro idex erro.
+lista5.pop(2)
+print(lista5)
+
+# Podemos remover todos os elementos
+print(lista5)
+lista5.clear()
+print(lista5)
+
+# Podemos repetir uma elemenos em uma lista
+nova = [1, 2, 3]
+print(nova)
+nova = nova *3
+print(nova)
+
+# Podemos facilmente converter um string para um lista
+
+# Exemplo 1
+
+curso = 'Programação em Python Essencial'
+print(curso)
+curso = curso.split()
+print(curso)
+
+# Obs: por padrão o split separa os elementos da lista pelo espaço entre elas
+# Exemplo 2
+curso = 'programação,em,Python,Essencial'
+print(curso)
+curso = curso.split(',')
+print(curso)
+
+# Convertendo uma lista em uma string
+
+lista6 = ['Programaçã', 'em', 'Python', 'Essencial']
+print(lista6)
+# Abaixo estamos falando pega  a lista6 coloca espaço em cada elemento e transforma em uma string
+
+curso = ' '.join(lista6)
+print(curso)
+
+# Abaixo estamos falando pega  a lista6 coloca $ em cada elemento e transforme numa string
+curso = '$'.join(lista6)
+print(curso)
+
+# Podemos realmente colocar qualquer tipo de dado em uma lista
+
+lista6 = [, 2.34, True, 'Geek', 'd', [1, 2, 3]]
+print(type(lista6))
+
+# Iterando sobre listas
+
+# Exemplo 1 - Utilizando for
+
+soma = 0
+for elemento in lista4:
+    print(elemento)
+    soma = soma + elemento
+print(soma)
+
+# Exemplo 2 - Utilizando while
+
+carrinho = []
+produto = ''
+
+while produto != 'sair':
+    print("Adicione um produto na lista ou digite 'sair': ")
+    produto = input()
+    if produto != 'sair':
+        carrinho.append(produto)
+
+for produto in carrinho:
+    print(produto)
+
+# Utilizando variávies em listas
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+
+numeros = [num1, num2, num3, num4, num5]
+print(numeros)
+
+# Fazemos acesso aos elementos de forma indexada
+
+cores = [ 'verde', 'amarelo', 'azul', 'branco']
+
+print(cores[0]) # verde
+print(cores[1]) # amarelo
+print(cores[2]) # azul
+print(cores[3]) # branco
+
+# Fazer acesso aos elementos de forma indexda inversa
+# Para entender melhor o índice pense na lista como um circulo,
+# onde o final de um elemento está ligado ao início da lista
+
+print(cores[-1]) # branco
+print(cores[-2]) # azul
+print(cores[-3]) # amarelo
+print(cores[-4]) # verde
+#print(cores[-5]) # erro pois não existe indice -5
+
+for cor in cores:
+    print(cor)
+
+indice = 0
+while indice < len(cores):
+    print(cores[indice])
+    indice = indice +1
+
+# Gerar indice em um for
+for indice, cor in enumerate(cores):
+    print(indice, cor)
+
+# Lista aceitam valore repetidos
+lista = []
+lista.append(42)
+lista.append(42)
+lista.append(33)
+lista.append(33)
+lista.append(42)
+
+print(lista)
+
+# outros métodos não tão importantes, mas também úteis
+
+# Encontrar o índice de um elemento na lista
+
+numeros = [5, 6, 7, 5,  8, 9, 10]
+
+# Em qual índice está o valor 6?
+print(numeros.index(6))
+
+# Em qual índice está o valor 9?
+print(numeros.index(9))
+
+# OBS: Caso não tenha este elemento na lista será apresentado erro
+# Em qual índice está o valor 19?
+# print(numeros.index(19)) # gera valueError
+
+# OBS: retorna o índice do primeiro elemento encontrado
+print(numeros.index(5))
+
+# Podemos fazer busca dentro de um range,ou seja, qual índice começar a buscar
+print(numeros.index(5, 1)) # Buscando a partir do índice 1
+print(numeros.index(5, 2)) # Buscando a partir do índice 2
+print(numeros.index(5, 3)) # Buscando a partir do índice 3
+#print(numeros.index(5, 4)) # Buscando a partir do índice 4
+
+# Podemos fazer busca dentro de um range início/fim
+print(numeros.index((8, 3, 6))) # Buscar o índice do valor entre os indices 6 e 8
+
+"""
+
